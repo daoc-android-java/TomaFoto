877bd1ec10e7393a87fc50e0ffcf99d20b3eeb72
Diego <diego.ordonez@gmail.com>
2021-06-14 15:30:34 -0500

    Update

--- app/src/main/java/ec/edu/ute/tomafoto/MainActivity.java
+++ app/src/main/java/ec/edu/ute/tomafoto/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.Intent;
@@ -13,17 +14,24 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StrictMode;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
-Recupera un pequeño preview de la foto tomada
+ * Usar camara del sistema
+ * https://developer.android.com/training/camera/photobasics
  */
 public class MainActivity extends AppCompatActivity {
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +39,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void tomarFoto(View view) {
+    /**
+     Recupera un pequeño preview de la foto tomada
+     */
+    public void tomarPreview(View view) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, 1);
     }
 
+    /**
+     Toma foto y la guarda en Public storage
+     (tambin recupera el preview)
+     */
+    public void guardarFoto(View view) {
+        URI picturesDirUri = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toURI();
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        String mimeType = "image/jpg";
+        String filename = "JPG_"+System.currentTimeMillis() + ".jpg";
+        intent.setType(mimeType);
+        intent.putExtra(Intent.EXTRA_TITLE, filename);
+        intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, picturesDirUri);
+        startActivityForResult(intent, 2);
+    }
+
+    /**
+     Aqui se recupera el preview y se toma la foto
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -46,6 +77,17 @@ public class MainActivity extends AppCompatActivity {
             ImageView imageView = findViewById(R.id.imageView);
             imageView.setImageBitmap(bmp);
         }
+
+        if(resultCode == RESULT_OK && requestCode == 2) {
+            //Toma la foto tamaño completo
+            Uri uri = data.getData();
+            System.out.println(uri);
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+            intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            startActivityForResult(intent, 3);
+        }
     }
 
 }
+
