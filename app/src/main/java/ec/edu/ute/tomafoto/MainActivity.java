fa0decb6a5e026319919098c10623b3daf5f55b0
Diego Ordóñez <diego.ordonez@gmail.com>
2021-07-20 12:52:18 -0500

    Update

--- app/src/main/java/ec/edu/ute/tomafoto/MainActivity.java
+++ app/src/main/java/ec/edu/ute/tomafoto/MainActivity.java
@@ -1,8 +1,10 @@
 package ec.edu.ute.tomafoto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.documentfile.provider.DocumentFile;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -21,11 +23,12 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * Usar camara del sistema
+ * Usar cámara del sistema
  * https://developer.android.com/training/camera/photobasics
  */
 public class MainActivity extends AppCompatActivity {
-    static String currentPhotoPath;
+    static final int REQ_FILE_LOCATION = 1;
+    static final int REQ_TAKE_PICT = 2;
     static Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,18 +37,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     Recupera un pequeño preview de la foto tomada
-     */
-    public void tomarPreview(View view) {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, 1);
-    }
-
-    /**
-     Toma foto y la guarda en Public storage
+     Toma foto y la guarda en una ubicación definida por el usuario
      https://developer.android.com/training/data-storage/shared/documents-files
      */
     public void guardarFoto(View view) {
+        //Intent para solicitar creación de un nuevo documento para foto
         Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
 
@@ -54,65 +50,43 @@ public class MainActivity extends AppCompatActivity {
 
         String filename = "JPG_"+System.currentTimeMillis() + ".jpg";
         intent.putExtra(Intent.EXTRA_TITLE, filename);
-
-        Uri uri = Uri.fromFile(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES));
-        System.out.println("(1) " + uri);
-        intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, uri);
-
-        startActivityForResult(intent, 2);
+        //Pide confirmación al usuario sobre dónde guardar la foto
+        startActivityForResult(intent, REQ_FILE_LOCATION);
     }
 
-    /**
-     Aqui se recupera el preview y se toma la foto
-     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        if(resultCode == RESULT_OK && requestCode == 1) {
-            //recupera un "preview" de la foto
-            if(data != null && data.getExtras() != null) {
-                Bitmap bmp = (Bitmap) data.getExtras().get("data");
-                ImageView imageView = findViewById(R.id.imageView);
-                imageView.setImageBitmap(bmp);
-            } else {
-
-                Toast.makeText(this, "RESULT_OK pero Intent.Bundle == null", Toast.LENGTH_LONG);
+        if(resultCode == RESULT_OK) {
+            if (requestCode == REQ_FILE_LOCATION) {
+                //Recupera la ubicación que el usuario confirmó para guardar ahí la foto
+                imageUri = data.getData();
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                //OJO: segunda llamada para tomar la foto
+                startActivityForResult(intent, REQ_TAKE_PICT);
+            } else if (requestCode == REQ_TAKE_PICT) {
+                //Presenta la imagen tomada en la pantalla
+                try {
+                    Toast.makeText(this, imageUri.toString(), Toast.LENGTH_LONG).show();
+                    System.out.println("(Ubicación de la imagen) " + imageUri);
+                    ImageView imageView = findViewById(R.id.imageView);
+                    //Recupera la imagen del disco
+                    //imageView.setImageURI(imageUri); //Esto es lo más directo, pero... si la imagen es muy grande NO carga
+                    InputStream is = getContentResolver().openInputStream(imageUri);
+                    Bitmap bmp = BitmapFactory.decodeStream(is);
+                    is.close();
+                    //Redimensiona la imagen para que cargue sin problemas
+                    int scrWidth = Resources.getSystem().getDisplayMetrics().widthPixels - 20; //20 es un padding cualquiera
+                    float imgRate = (float) bmp.getWidth() / bmp.getHeight();
+                    Bitmap resizedBmp = Bitmap.createScaledBitmap(bmp, scrWidth, (int) (scrWidth / imgRate), false);
+                    imageView.setImageBitmap(resizedBmp);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
-
-        if(resultCode == RESULT_OK && requestCode == 2) {
-            //Toma la foto tamaño completo
-            Uri uri = data.getData();
-            imageUri = data.getData();
-            currentPhotoPath = uri.toString();
-            System.out.println("(2) " + imageUri);
-            System.out.println("(2) " + currentPhotoPath);
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
-            intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-            startActivityForResult(intent, 3);
-        }
-
-        if(resultCode == RESULT_OK && requestCode == 3) {
-            try {
-                System.out.println("(3) " + imageUri);
-                ImageView imageView = findViewById(R.id.imageView);
-                //imageView.setImageURI(imageUri); //Si la imagen es muy grande NO carga
-                InputStream is = getContentResolver().openInputStream(imageUri);
-                Bitmap bmp = BitmapFactory.decodeStream(is);
-                is.close();
-                System.out.println("W: " + bmp.getWidth() + " H: " + bmp.getHeight());
-                float rate = (float)bmp.getWidth() / bmp.getHeight();
-                System.out.println("Rate: " + rate);
-                Bitmap resizedBmp = Bitmap.createScaledBitmap(bmp, 480, (int)(480/rate), false);
-                imageView.setImageBitmap(resizedBmp);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        }
-
     }
 
 }
